e54523b080733aa76980d60b7b093fb3146b9845
1i7 <ii1i7@yandex.ru>
2017-02-11 23:04:50 +0300

    Добавить BabblerDebugPanel в число основных виджетов

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export BabblerConnectionErrorSnackbar from './BabblerConnectionErrorSnackbar';
 export BabblerConnectionPanel from './BabblerConnectionPanel';
 export BabblerConnectionStatusIcon from './BabblerConnectionStatusIcon';
 export BabblerDataFlow from './BabblerDataFlow';
+export BabblerDebugPanel from './BabblerDebugPanel';
 export SerialPortDropdown from './SerialPortDropdown';
 export SerialPortPopover from './SerialPortPopover';
 
