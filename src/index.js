dae04205f4215d7606505de4465c1a8b1378760f
1i7 <ii1i7@yandex.ru>
2017-07-22 17:18:03 +0300

    Виджет BabblerCmdQueueStatus - состояние очереди команд

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export BabblerConnectionErrorSnackbar from './BabblerConnectionErrorSnackbar';
 export BabblerConnectionPanel from './BabblerConnectionPanel';
 export BabblerConnectionStatusIcon from './BabblerConnectionStatusIcon';
+export BabblerCmdQueueStatus from './BabblerCmcQueueStatus';
 export BabblerDataFlow from './BabblerDataFlow';
 export BabblerDebugPanel from './BabblerDebugPanel';
 export SerialPortDropdown from './SerialPortDropdown';
